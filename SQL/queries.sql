d7434f9dfb1c55c2316694ef58f7bb0cfb78850b
Ali Mirzakhalili <alimknu@gmail.com>
2025-03-29 20:59:21 -0400

    Completed 2e.

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -25,3 +25,16 @@ GROUP BY HotelChain.chainName;
 SELECT *
 FROM Booking
 WHERE customerID = 'TC001';
+
+-- INDEXES
+-- This index allows us to quickly check the duration for which a room is booked. This would allow us to quickly find out whether or not a room is available for a given date range.
+-- It is expected to be used in queries checking room availability, which is a common operation in hotel management systems. Example: "Find rooms not booked between 2025-01-01 and 2025-01-10"
+CREATE INDEX idx_booking_dates ON Booking (startDate, endDate);
+
+-- This index allows us to search for rooms with a specific view within a specific price range. This is useful for queries where customers only want to see one type of view and have a specific budget.
+-- Example: "Find rooms with a sea view under $250"
+CREATE INDEX idx_room_view_price ON Room (view, price);
+
+-- This index allows us to quickly find hotels by their ratings. This is useful for customers who want to look through hotels in a specific rating range.
+-- Example: "Find hotels with a rating of 4 stars or higher"
+CREATE INDEX idx_hotel_rating ON Hotel (rating);
